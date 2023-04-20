c4875b083b20483970d03c11cc07265782a9c5ac
Luizlmorim <112559054+luizlmorim@users.noreply.github.com>
2023-04-20 12:02:05 -0300

    Add files via upload

---
+++ aula06/lista01py.py
@@ -0,0 +1,8 @@
+lista_compras = ['banana', 'laranja', 'maçã']
+for i in lista_compras: #cada item da lista o for joga no "i", em seguida imprime
+    print(i)
+
+#exemplo:
+'''lista_compras.insert(1,'carro') - insere um item na posição indicada '''
+
+'''del lista_compras '''
---
+++ aula06/lista02.py
@@ -0,0 +1,14 @@
+lista_alunos = []
+n = int(input("Digite a quantidade de alunos: "))
+for a in range(n):
+    lista_alunos.append(input("Digite o nome do aluno: "))
+for a in range(a): #usamos um novo for para pecorrer a lista e imprimir cada item, não a lisat em si.
+     print(a)
+
+'''Ateração - mostarndo o indice e o item'''
+lista_alunos = []
+n = int(input("Digite a quantidade de alunos: "))
+for a in range(n):
+    lista_alunos.append(input("Digite o nome do aluno: "))
+for a in range(a): #usamos um novo for para pecorrer a lista e imprimir cada item, não a lisat em si.
+     print(a, lista_alunos[a]) #mostrando o nome e o índice. a se referindo ao indice
