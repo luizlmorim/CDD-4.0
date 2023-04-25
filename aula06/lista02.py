c9a471fcbcdfd033ffab561f99cbda1172a0d7b7
Luizlmorim <112559054+luizlmorim@users.noreply.github.com>
2023-04-25 08:18:03 -0300

    Update lista02.py

--- aula06/lista02.py
+++ aula06/lista02.py
@@ -3,12 +3,12 @@ n = int(input("Digite a quantidade de alunos: "))
 for a in range(n):
     lista_alunos.append(input("Digite o nome do aluno: "))
 for a in range(a): #usamos um novo for para pecorrer a lista e imprimir cada item, não a lisat em si.
-     print(a)
+     print(a, lista_alunos[a])
 
 '''Ateração - mostarndo o indice e o item'''
 lista_alunos = []
 n = int(input("Digite a quantidade de alunos: "))
 for a in range(n):
     lista_alunos.append(input("Digite o nome do aluno: "))
-for a in range(a): #usamos um novo for para pecorrer a lista e imprimir cada item, não a lisat em si.
+for a in range(n): #usamos um novo for para pecorrer a lista e imprimir cada item, não a lisat em si.
      print(a, lista_alunos[a]) #mostrando o nome e o índice. a se referindo ao indice
